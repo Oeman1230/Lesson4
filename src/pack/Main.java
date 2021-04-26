deee9639c41b9af4613c3f5e9c2d6b712c4198c9
Oeman1230 <moskalev.iura@mail.ru>
2021-04-26 22:56:10 +0300

    finished everything

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -2,8 +2,10 @@ package pack;
 
 import pack.classOfBoysAndGirls.ClassOfPeople;
 import pack.classOfBoysAndGirls.Clothes;
+import pack.classOfBoysAndGirls.ClothesColour;
 import pack.classOfBoysAndGirls.Person;
 
+import java.awt.*;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,28 +16,44 @@ public class Main {
     public static void main(String[] args) {
         ClassOfPeople classOfPeople = new ClassOfPeople();
         classOfPeople.CreateClassOfPeopleByCountAndQuantity(40, 30, 70);
-        System.out.println("boys:" + classOfPeople.getBoysCount() + " girls:" + classOfPeople.getGirlsCount());
+        System.out.println("boys:" + classOfPeople.getBoysCount() +
+                " girls:" + classOfPeople.getGirlsCount());
         System.out.println("==============================================");
         Double averageAge = 0.0;
-        for(int i = 0; i < classOfPeople.getPeople().size() - 1; i++ )
-        {
-                averageAge += classOfPeople.getPeople().get(i).getPersonAge();
+        for (int i = 0; i < classOfPeople.getPeople().size() - 1; i++) {
+            averageAge += classOfPeople.getPeople().get(i).getPersonAge();
         }
 
-        System.out.println("average age " +averageAge/classOfPeople.getPeople().size());
+        System.out.println("average age " + averageAge / classOfPeople.getPeople().size());
         System.out.println("==============================================");
 
         Comparator<Person> comClothName = Comparator.comparing(Clothes::getClothName);
 
         Stream<Person> clothesUp = classOfPeople.getPeople().stream().sorted(comClothName);
 
-       List<Person> listClothName = clothesUp.collect(Collectors.toList());
-        listClothName.forEach(it ->  System.out.println(it.getPersonName() + " "+it.getPersonGender() + " " + it.getClothName()));
+        List<Person> listClothName = clothesUp.collect(Collectors.toList());
+        listClothName.forEach(it -> System.out.println(it.getPersonName() +
+                " " + it.getPersonGender() + " " + it.getClothName()));
         System.out.println("==============================================");
         Comparator<Person> comClothSize = Comparator.comparing(Clothes::getClothSize);
 
-        Stream<Person> clothesSize = classOfPeople.getPeople().stream().sorted(comClothSize.reversed());
+        Stream<Person> clothesSize = classOfPeople.getPeople()
+                .stream().sorted(comClothSize.reversed());
         List<Person> listClothSize = clothesSize.collect(Collectors.toList());
-        listClothSize.forEach(it -> System.out.println(it.getPersonName()+ " " + it.getPersonGender() + " " + it.getClothSize()));
+        listClothSize.forEach(it -> System.out.println(it.getPersonName() +
+                " " + it.getPersonGender() + " " + it.getClothSize()));
+        System.out.println("==============================================");
+        Stream<Person> pocketCountSt = classOfPeople.getPeople()
+                .stream().filter(it -> it.getClothPocketsCount() == 2);
+        List<Person> listPockets = pocketCountSt.collect(Collectors.toList());
+        listPockets.forEach(it -> System.out.println(it.getPersonName() +
+                " " + it.getPersonGender() + " " + it.getClothPocketsCount()));
+        System.out.println("==============================================");
+        Stream<Person> colourSt = classOfPeople.getPeople().stream()
+                .filter(it -> it.getClothColour() == ClothesColour.BROWN | it.getClothColour() == ClothesColour.GREEN);
+        List<Person> listColour = colourSt.collect(Collectors.toList());
+        listColour.forEach(it -> System.out.println(it.getPersonName()
+                + " " + it.getPersonGender() + " " + it.getClothColour()
+        ));
     }
 }
